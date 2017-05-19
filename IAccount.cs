09808b134c0eefd5abc0ce1f6f574c1ef7dcc96c
SefikaBoz <sefikaboz12@gmail.com>
2017-05-19 18:32:37 +0300

    Create IAccount.cs

---
+++ IAccount.cs
@@ -0,0 +1,16 @@
+# 19-05-2017---2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PErson
+{
+    interface IAccount
+    {
+        int AccountNumber { get; set; }
+        int AccountAmount { get; set; }
+    }
+}
