39efaa261baa8d9c01f02a2c2f7054f1d62651bc
SefikaBoz <sefikaboz12@gmail.com>
2017-05-19 18:33:34 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+# 19-05-2017---2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PErson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student
+            {
+                FirstAndLastName = "William Smith",
+                EPosta = "william@windowslive.com",
+                ExamGrade = 50,
+                Phone = ""
+            };
+            Lecturer lec = new Lecturer
+            {
+                FirstAndLastName = "Steve",
+                Department = "IT",
+                Task = "Software Developer"
+            };
+            Console.WriteLine("Name : {0} , Department : {1}, Task : {2} " , lec.FirstAndLastName, lec.Department,lec.Task);
+            Console.ReadLine();
+        }
+    }
+}
